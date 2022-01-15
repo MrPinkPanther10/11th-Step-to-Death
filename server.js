c262a83429a6c02008ddae412c3f8bf6fdd74f35
MrPinkPanther10 <90013822+MrPinkPanther10@users.noreply.github.com>
2022-01-15 15:55:51 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-// Necessary Modules
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var notes = require("./db/db.json")
+// add required modules
+const express = require("express");
+const path = require("path");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+// Create server application at port 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Read URL or JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Include js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Use public folder
+app.use(express.static("public"));
+
+// Add listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
